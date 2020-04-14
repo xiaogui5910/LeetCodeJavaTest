c8cb6b1e76d6839ee018d8dbba0edda9ee464b80
chenchenggui <chenchenggui@qie.tv>
2020-04-14 10:16:54 +0800

    add ValidParentheses other solution

--- src/main/java/leetcode/ValidParentheses.java
+++ src/main/java/leetcode/ValidParentheses.java
@@ -78,6 +78,31 @@ public class ValidParentheses {
         return result && list.size() == 0;
     }
 
+    /**
+     * 解法2：
+     * 题意是判断括号匹配是否正确，很明显，我们可以用栈来解决这个问题，当出现左括号的时候入栈，当遇到右括号时，
+     * 判断栈顶的左括号是否何其匹配，不匹配的话直接返回 false 即可，最终判断是否空栈即可，这里我们可以用数组模拟栈的操作使其操作更快，
+     * 有个细节注意下 top = 1;，从而省去了之后判空的操作和 top - 1 导致数组越界的错误。
+     * @param s 字符串（括号）
+     * @return 是否匹配正确
+     */
+    public boolean solution1(String s) {
+        char[] stack = new char[s.length() + 1];
+        int top = 1;
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack[top++] = c;
+            } else if (c == ')' && stack[--top] != '(') {
+                return false;
+            } else if (c == ']' && stack[--top] != '[') {
+                return false;
+            } else if (c == '}' && stack[--top] != '{') {
+                return false;
+            }
+        }
+        return top == 1;
+    }
+
     public void test() {
 //        String s = "()[]{}";
 //        String s = "([)]";
@@ -92,4 +117,5 @@ public class ValidParentheses {
         Utils.printNum(this, result);
 
     }
+
 }
